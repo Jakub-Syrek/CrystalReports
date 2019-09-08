9a780e2cc103a910a97e1dfdaaefea5c1100011e
Jakub Syrek <jaqb.syrek@gmail.com>
2019-09-08 20:06:05 +0200

    6

---
+++ CrystalReportsEntity1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CrystalReportsEntity1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReportEntityV1 = new CrystalReportsEntity1.CrystalReportEntityV();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = 0;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.ReportSource = "C:\\Users\\jaqbs\\source\\repos\\CrystalReportsSolution\\CrystalReportsEntity1\\CrystalR" +
+    "eportDataSet.rpt";
+            this.crystalReportViewer2.Size = new System.Drawing.Size(1317, 1070);
+            this.crystalReportViewer2.TabIndex = 0;
+            this.crystalReportViewer2.Load += new System.EventHandler(this.crystalReportViewer2_Load);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1317, 1070);
+            this.Controls.Add(this.crystalReportViewer2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        //private CrystalReport1 CrystalReport11;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        //private CrystalReportEntity CrystalReportEntity1;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer3;
+        //private CrystalReportEntityV CrystalReportEntityV1;
+        //private CrystalReportEntityV CrystalReportEntityV3;
+        //private CrystalReportEntityV CrystalReportEntityV2;
+        //private CrystalReport4 CrystalReport41;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+       // private CrystalReport4 crystalReport4;
+        private CrystalReport4 CrystalReport41;
+        private CrystalReportDataSet CrystalReportDataSet1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        private CrystalReportEntityV CrystalReportEntityV1;
+    }
+}
+
---
+++ CrystalReportsEntity1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using CrystalDecisions.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CrystalReportsEntity1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CrystalReportDataSet crystalReport = new CrystalReportDataSet();
+            var path = @"C:\Users\jaqbs\source\repos\CrystalReportsSolution\CrystalReportsEntity1\CrystalReportDataSet.rpt";
+            crystalReport.Load(path);
+            crystalReportViewer2.ReportSource = crystalReport;
+            crystalReportViewer2.Refresh();
+
+        }
+
+       
+
+       
+
+
+
+        //private void crystalReportViewer1_Load(object sender, EventArgs e)
+        //{
+            
+
+        //    CrystalReport4 rpt = new CrystalReport4();
+
+            
+        //    DataSet1 db = new DataSet1();
+
+        //    rpt.SetDataSource(from employee in db.Employees.Take(100)
+        //                                select employee.FirstName );
+
+
+        //    //rpt.SetDataSource(db);
+
+        //    //rpt.SetDataSource(from u in db.Employees
+        //    //                      select new
+        //    //                      {
+        //    //                          u.EmployeeID,
+        //    //                          u.FirstName,
+        //    //                          u.LastName
+        //    //                      });
+            
+        //   // crystalReportViewer1.ReportSource = rpt;
+        //    crystalReportViewer1.RefreshReport();        
+        //}
+
+        private void crystalReportViewer2_Load(object sender, EventArgs e)
+        {
+            DataSet1 db = new DataSet1();
+
+            DataSet dsReport = new DataSet();
+
+            var EmployeeTable = db.Employees.Copy();
+            var OrdersTable = db.Orders.Copy();
+
+
+            dsReport.Tables.Add(EmployeeTable);
+            dsReport.Tables.Add(OrdersTable);
+
+            CrystalReportDataSet crystalReport = new CrystalReportDataSet();
+            //var path = @"C:\Users\jaqbs\source\repos\CrystalReportsSolution\CrystalReportsEntity1\CrystalReportDataSet.rpt";
+            //crystalReport.Load(path);
+            crystalReport.SetDataSource(dsReport);
+            CrystalReportViewer rv = new CrystalReportViewer();
+            rv.ReportSource = crystalReport;
+            crystalReportViewer2.ReportSource = crystalReport;
+            crystalReportViewer2.Refresh();
+
+            //rpt.SetDataSource(from employee in db.Employees.Take(100)
+            //                  select employee.FirstName);
+
+
+        }
+    }
+}
